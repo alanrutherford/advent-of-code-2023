c71fa55f685089d16142d244f720ced3032a76a2
Alan Rutherford <alan@propelleraero.com.au>
2023-12-13 22:03:56 +1100

    🧹

--- src/day13/partA.ts
+++ src/day13/partA.ts
@@ -10,7 +10,6 @@ export default function partA(): void {
     .map((pattern) => pattern.split("\n"));
   let score = 0;
   const isColumnReflection = (puzzle: string[], columnNumber: number) => {
-    console.log(columnNumber);
     for (
       let i = 0;
       columnNumber + 1 + i < puzzle[0].length && columnNumber - i >= 0;
@@ -37,11 +36,9 @@ export default function partA(): void {
     return true;
   };
   for (let i = 0; i < input.length; i++) {
-    console.log(`${i}: ---`);
     for (let j = 0; j < input[i].length - 1; j++) {
       if (input[i][j] === input[i][j + 1]) {
         if (isRowReflection(input[i], j)) {
-          console.log(`${i} rowReflection ${j}`);
           score += 100 * (j + 1);
         }
       }
@@ -49,23 +46,12 @@ export default function partA(): void {
     for (let j = 0; j < input[i][0].length; j++) {
       const column1 = input[i].map((line) => line[j]).join("");
       const column2 = input[i].map((line) => line[j + 1]).join("");
-
       if (column1 === column2) {
         if (isColumnReflection(input[i], j)) {
-          console.log(`${i} columnReflection ${j}`);
-          console.log(column1);
           score += j + 1;
         }
-        //check if proper rflection
-        //column add
       }
-      // if (checkIfClearOfGalaxy(column)) {
-      //   for (let j = 0; j < input.length; j++) {
-      //     input[j].splice(i, 0, ".");
-      //   }
-      //   i++;
-      // }
     }
   }
-  console.log(`Part A: ${score} ${score !== 33744}`);
+  console.log(`Part A: ${score}`);
 }
